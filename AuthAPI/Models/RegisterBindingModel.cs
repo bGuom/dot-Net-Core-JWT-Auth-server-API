7ac98992acde704ab9a1ab52ba1f6ec59cc9869d
bguom <buddhi@theoftwarepractice.com>
2019-11-03 22:30:12 +0530

    JWT Token Authorization API
    - User Registration
    - Get Token
    -Get Logged In User Info

---
+++ AuthAPI/Models/RegisterBindingModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthAPI.Models
+{
+    public class RegisterBindingModel
+    {
+            [Required]
+            [StringLength(25, ErrorMessage = "{0} length must be between {2} and {1}.", MinimumLength = 4)]
+            public string Username { get; set; }
+
+            [Required(ErrorMessage = "Email is a required field")]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Required(ErrorMessage = "Password is a required field")]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+
+
+        
+    }
+}
